3964f42654a65149178030e1b1f1cd1b5f38c48c
Ivan00100 <ikonstandoglo6@gmail.com>
2024-10-23 20:08:42 +0300

    24.1-test

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Content() {
+  return (
+    <div id="content" className="content">
+      <p>Select a category to view data.</p>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <button className="btn btn-primary m-2">People</button>
+      <button className="btn btn-primary m-2">Planets</button>
+      <button className="btn btn-primary m-2">Vehicles</button>
+    </div>
+  );
+}
+
+export default Navbar;
