dce1921577571ae39cb5328ab290d75a3fbf9b0f
Ivan00100 <ikonstandoglo6@gmail.com>
2024-10-24 18:42:49 +0300

    homework-24.1-main

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
       <header className="App-header">
         <h1>StarWars Info</h1>
       </header>
+
+      <div className='img-wrapp'>
+        <img src="https://i0.wp.com/bgn2018media.s3.amazonaws.com/wp-content/uploads/2017/09/29000545/sw-the-last-jedi-tall-B.jpg?fit=1536%2C864&ssl=1" alt="Star Wars" className="banner"></img>
+      </div>
+      
       <Navbar />
       <Content />
     </div>
